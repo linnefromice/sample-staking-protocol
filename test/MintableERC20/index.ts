298df19d5e82fdf5293198787d47f10925bcc824
linnefromice <linnefromice@gmail.com>
2022-05-06 15:56:15 +0900

    update test

--- test/MintableERC20/index.ts
+++ test/MintableERC20/index.ts
@@ -11,10 +11,10 @@ describe("MintableERC20", () => {
     const token = await new MintableERC20__factory(owner).deploy(_name, _symbol)
     await token.deployTransaction.wait()
     
-    expect(_name, await token.name())
-    expect(_symbol, await token.symbol())
-    expect("18", (await token.decimals()).toString())
-    expect("0", (await token.totalSupply()).toString())
+    expect(_name).to.eq(await token.name())
+    expect(_symbol).to.eq(await token.symbol())
+    expect("18").to.eq((await token.decimals()).toString())
+    expect("0").to.eq((await token.totalSupply()).toString())
   })
 
   it("Should be mintable by anyone", async () => {
@@ -25,9 +25,9 @@ describe("MintableERC20", () => {
     let tx;
     tx = await token.connect(userA).mint(ethers.utils.parseUnits("1.25"))
     await tx.wait()
-    expect("1.25", ethers.utils.formatUnits(await token.totalSupply()))
+    expect("1.25").to.eq(ethers.utils.formatUnits(await token.totalSupply()))
     tx = await token.connect(userB).mint(ethers.utils.parseUnits("20.68"))
     await tx.wait()
-    expect("21.93", ethers.utils.formatUnits(await token.totalSupply()))
+    expect("21.93").to.eq(ethers.utils.formatUnits(await token.totalSupply()))
   })
 })
