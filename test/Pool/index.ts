3d10ce984779863d4c774898922afa786f51cbce
linnefromice <linnefromice@gmail.com>
2022-05-15 10:24:11 +0900

    test(contracts): Pool#withdraw test (success case)

--- test/Pool/index.ts
+++ test/Pool/index.ts
@@ -84,6 +84,34 @@ describe("Pool", () => {
   })
 
   describe("Enable to withdraw & get back tokens", () => {
+    it("success", async () => {
+      const [owner, player] = await ethers.getSigners();
+      const { token, rewardToken, pool } = await setup(owner)
+
+      let tx;
+
+      // Prerequisites
+      // - mint mock token
+      tx = await token.connect(player).mint(ethers.utils.parseEther("0.05"), { from: player.address })
+      await tx.wait()
+      // - deposit
+      tx = await token.connect(player).approve(pool.address, ethers.utils.parseEther("0.02"), { from: player.address })
+      await tx.wait()
+      tx = await pool.connect(player).deposit(ethers.utils.parseEther("0.02"), { from: player.address })
+      await tx.wait()
+
+      // withdraw
+      // - approve
+      tx = await rewardToken.connect(player).approve(pool.address, ethers.utils.parseEther("0.015"), { from: player.address })
+      await tx.wait()
+      // - execute
+      tx = await pool.connect(player).withdraw(ethers.utils.parseEther("0.015"), { from: player.address })
+      await tx.wait()
+
+      expect(ethers.utils.parseEther("0.045")).to.eq((await token.balanceOf(player.address)).toString())
+      expect(ethers.utils.parseEther("0.005")).to.eq((await rewardToken.balanceOf(player.address)).toString())
+    })
+
     it("failure: amount is over deposited", async () => {
       const [owner, player] = await ethers.getSigners();
       const { token, rewardToken, pool } = await setup(owner)
